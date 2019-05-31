31781677aeb1b9fa8f72cc1b02a7d94c003b8a97
Dan-Vy Le <danvyle85@gmail.com>
2019-05-31 20:16:10 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -1,6 +1,7 @@
 require 'pry'
 
-songs = ["Phoenix - 1901",
+songs = [
+  "Phoenix - 1901",
   "Tokyo Police Club - Wait Up",
   "Sufjan Stevens - Too Much",
   "The Naked and the Famous - Young Blood",
@@ -12,28 +13,49 @@ songs = ["Phoenix - 1901",
 ]
 
 def help
-  puts "I accept the following commands:
-- help : displays this help message
-- list : displays a list of songs you can play
-- play : lets you choose a song to play
-- exit : exits this program"
+  puts "I accept the following commands:"
+  puts "- help : displays this help message"
+  puts "- list : displays a list of songs you can play"
+  puts "- play : lets you choose a song to play"
+  puts "- exit : exits this program"
 end
 
 def list(songs)
-  puts songs
-  
+  songs.each_with_index do |song, number|
+    puts "#{number + 1}: #{song}}"
+  end
 end
 
 def play(songs)
   puts "Please enter a song name or number:"
   song_choice = gets.chomp
-    songs.each_with_index do |song|
-      if song_choice >= 0 || song_choice <= 8
-        puts "PLaying song[index]"
-      elsif song_choice == song
+      if (1..9).to_a.include?(song_choice.to_i)
+        puts "Playing #{songs[song_choice.to_i - 1]}"
+        # binding.pry
+      elsif songs.include?(song_choice)
         puts "Playing #{song_choice}"
-        binding.pry
       else
         puts "Invalid input, please try again"
       end
+end
+
+def exit_jukebox
+  puts "Goodbye"
+end
+
+def run(songs)
+  help
+  puts "Please enter a command:"
+    entry = gets.chomp
+    if entry.downcase == "help"
+      help
+    elsif entry.downcase == "list"
+      list(songs)
+    elsif entry.downcase == "play"
+      play(songs)
+    elsif entry.downcase == "exit"
+      exit_jukebox
+    else
+      help
+    end
 end
